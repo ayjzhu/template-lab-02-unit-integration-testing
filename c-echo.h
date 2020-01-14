edf4030fbacdc202315d2aeec455b5aeb7752a2f
ayjzhu <ayjzhu@gmail.com>
2020-01-14 10:11:54 -0800

    renamed c-echo.cpp to c-echo.h

--- c-echo.h
+++ c-echo.h
@@ -1,13 +1,13 @@
 #include <iostream>
 
-int main(int argv, char** argc){
-	// Skip the first argc index because its the program
-	for(int i = 1; i< argv; i++){
-		std::cout << argc[i];
-		// Print a whitespace after all but the last iteration
-		if(i < argv -1 ){
-			std::cout << " ";
-		}
-	}
-	std::cout << std::endl;
+std::string echo(int length, char** chars) {
+    std::string ret = "";
+    for(int i = 1; i < length; i++) {
+        ret += chars[i];
+        if(i < length - 1) {
+            ret += " ";
+        }
+    }
+    ret += "\n";
+    return ret;
 }
