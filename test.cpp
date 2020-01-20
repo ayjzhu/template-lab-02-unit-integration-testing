3d8c510f1c91a4925fc604be36be148450c3bb1e
ayjzhu <ayjzhu@gmail.com>
2020-01-20 15:17:30 -0800

    added three additional test cases

--- test.cpp
+++ test.cpp
@@ -12,6 +12,21 @@ TEST(EchoTest, EmptyString) {
     EXPECT_EQ("", echo(1,test_val));
 }
 
+TEST(EchoTest, CapitalizedString) {
+	char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "HELLO"; test_val[2] = "WORLD";
+	EXPECT_EQ("HELLO WORLD", echo(3, test_val));
+}
+
+TEST(EchoTest, QuotedString) {
+    char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "\"hello"; test_val[2] = "world\"";
+    EXPECT_EQ("\"hello world\"", echo(3,test_val));
+}
+
+TEST(EchoTest, BackslashedCharacter) {
+    char* test_val[4]; test_val[0] = "./c-echo"; test_val[1] = "hello"; test_val[2] = "\n"; test_val[3] = "world";
+    EXPECT_EQ("hello \n world", echo(4,test_val));
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
